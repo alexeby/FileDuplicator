1e44ff690fbc348d2dcb8cc80c3d96f7e42c82c6
Eby <20789914@Americas.OneACS.com>
2021-02-09 16:27:17 -0600

    Updated build batch file. Added requirements.txt for dependency tracking

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
-rmdir /s /q dist 
-pyinstaller --onefile main.py
-mkdir dist\in\
-mkdir dist\out\
-mkdir dist\setup\
-copy setup\conf.ini dist\setup
+rmdir /s /q dist ^
+ & venv\Scripts\activate.bat ^
+ && pyinstaller main.py -n="fileDuplicator" --onefile ^
+ && venv\Scripts\deactivate.bat ^
+ && mkdir dist\in\ ^
+ && mkdir dist\out\ ^
+ && mkdir dist\setup\ ^
+ && copy setup\conf.ini dist\setup
